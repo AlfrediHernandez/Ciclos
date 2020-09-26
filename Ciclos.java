8a27d273b76d1c3d2a629cf8adc601dcf03b121c
AlfrediHdz <alfredi1963@hotmail.com>
2020-09-26 10:26:09 -0500

    Se agrega programita a ciclos

---
+++ Ciclos.java
@@ -0,0 +1,11 @@
+//Programa que muestre los Numeros del 1 al 100 utilizando el ciclo Do While
+public class Ciclos{
+	public static void main(String [] ar){
+		int a = 1;
+		System.out.println("Numeros del 1 al 100: ");
+			do{
+				System.out.println(a);
+				a++;
+			}while(a <=25);
+	}
+}
